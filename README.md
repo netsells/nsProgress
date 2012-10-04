ffb84ab6d5c6909c43abe4535812648eef84d083
Sam Jordan <sam@netsells.co.uk>
2012-10-04 14:16:50 +0100

    Initial Commit
    I really need to add comments...
    
    Signed-off-by: Sam Jordan <sam@netsells.co.uk>

---
+++ README.md
@@ -0,0 +1,85 @@
+Based off the fantastic class by Sam Vermette on [SVProgressHUD](https://github.com/samvermette/SVProgressHUD). nsProgress aims to fill the gap by allowing websites and specifically phonegap apps a cross platform way of showing progress via a HUD. This plugin is written for jQuery so requires this to work (written with 1.8.2).
+
+As with SVProgressHUD, the success and error icons are from [Glyphish](http://glyphish.com/).
+
+## Simple Usage
+To get started, place a div in your source and call the plugin on it to initialise.
+
+```
+<div id="progress_modal"></div> 
+
+<script>
+    $(document).ready(function() {
+
+        // Default Settings
+        $('#progress_modal').nsProgress();
+
+        // Custom Settings
+        $('#progress_modal').nsProgress({
+            img_path: 'js/plugins/nsProgress'
+        });
+
+    });
+</script>
+```
+
+You are now ready to call methods on the plugin to display, change and hide the HUD.
+
+```
+<script>
+    $(document).ready(function() {
+
+        $('#progress_modal').nsProgress();
+        $('#progress_modal').nsProgress('showSuccessWithStatusAndMaskType', 'It Worked!', 'black');
+
+    });
+</script>
+```
+
+Which will display something like this:
+
+![Demo Result](http://f.cl.ly/items/3c110E0u2Z3r2n353E0s/Image%202012.10.04%2014:10:43.png)
+
+## HUD Options
+There are a few options that you can pass to choose the HUD Mask style, they are
+
+<table>
+    <tr>
+        <td>'none'</td>
+        <td>allow user interactions, don't dim background UI (default)</td>
+    </tr>
+    <tr>
+        <td>'clear'</td>
+        <td>disable user interactions, don't dim background UI</td>
+    </tr>
+    <tr>
+        <td>'black'</td>
+        <td>disable user interactions, dim background UI with 50% translucent black (my favourite)</td>
+    </tr>
+</table>
+
+## Methods Available
+I've tried to cover every possible combination, they should be self explanitory!
+
+```
+$('#progress_modal').nsProgress('show');
+$('#progress_modal').nsProgress('showWithMaskType', 'none');
+$('#progress_modal').nsProgress('showWithStatus', 'Loading&hellip;');
+$('#progress_modal').nsProgress('showWithStatusAndMaskType', 'Loading&hellip;', 'none');
+$('#progress_modal').nsProgress('showWithImage', 'nsLoader.gif');
+$('#progress_modal').nsProgress('showSuccessWithStatus', 'Error');
+$('#progress_modal').nsProgress('showErrorWithStatus', 'Success');
+$('#progress_modal').nsProgress('showSuccessWithStatusAndMaskType', 'Success', 'none');
+$('#progress_modal').nsProgress('showErrorWithStatusAndMaskType', 'Error', 'none');
+$('#progress_modal').nsProgress('showWithImageAndStatus', 'nsLoader.gif', 'Loading&hellip;');
+$('#progress_modal').nsProgress('showWithImageAndMaskType', 'nsLoader.gif', 'none');
+$('#progress_modal').nsProgress('showWithImageAndStatusAndMaskType', 'nsLoader.gif', 'Loading&hellip;', 'none');
+$('#progress_modal').nsProgress('dismiss');
+
+```
+
+## TODO
+Feel free to fork and add any of these if I don't get round to doing them :)
+
+* Delayed dissmissal message like SVProgressHUD has (eg, SVProgressHUD dismissWithError:@"Error Connecting")
+* More config settings
---
+++ nsProgress.js
@@ -0,0 +1,127 @@
+(function( $ ){
+
+    var methods = {
+        init : function(options) {
+
+            var defaults = {
+                img_path: 'nsProgress'
+            };
+
+            options = $.extend(defaults, options);
+            height_without_text = 60;
+            height_with_text = 80;
+
+            $(this).addClass('nsProgress_container');
+            $('body').append('<div class="nsProgress_block"></div>');
+
+            $(this).append('<img class="nsProgress_img" data-src="' + options.img_path + '" src="' + options.img_path + '/nsLoader.gif" /><div class="nsProgress_text">Loading&hellip;</div>');
+        },
+        show : function( ) { 
+            $(this).show();
+        },
+        showWithMaskType : function( type ) {
+
+            switch(type) {
+                case 'none':
+                    break;
+                case 'clear':
+                    $('.nsProgress_block').show();
+                    break;yep
+                case 'black':
+                    $('.nsProgress_block').show();
+                    $('.nsProgress_block').css('background', 'rgba(0, 0, 0, 0.5)');
+                    break;
+                default:
+                    break;
+            }
+
+            $(this).nsProgress('show');
+        },
+        showWithStatus : function( status ) {
+
+            $(this).css('height', height_with_text + 'px');
+            $(this).find('.nsProgress_text').html( status ).show();;
+            $(this).nsProgress('show');
+
+        },
+        showWithStatusAndMaskType : function ( status, type ) {
+
+            $(this).nsProgress('showWithMaskType', type);
+            $(this).nsProgress('showWithStatus', status);
+
+        },
+        showWithImage : function( image_name ) {
+
+            $(this).find('.nsProgress_img').attr('src', $(this).find('.nsProgress_img').attr('data-src') + '/' + image_name);
+            $(this).nsProgress('show');
+
+        },
+        showSuccessWithStatus : function ( status ) {
+
+            $(this).nsProgress('showWithImage', 'nsSuccess.png');
+            $(this).nsProgress('showWithStatus', status);
+
+        },
+        showErrorWithStatus : function ( status ) {
+
+            $(this).nsProgress('showWithImage', 'nsError.png');
+            $(this).nsProgress('showWithStatus', status);
+
+        },
+        showSuccessWithStatusAndMaskType : function ( status, type ) {
+
+            $(this).nsProgress('showWithImage', 'nsSuccess.png');
+            $(this).nsProgress('showWithStatusAndMaskType', status, type);
+
+        },
+        showErrorWithStatusAndMaskType : function ( status, type ) {
+
+            $(this).nsProgress('showWithImage', 'nsError.png');
+            $(this).nsProgress('showWithStatusAndMaskType', status, type);
+
+        },
+        showWithImageAndStatus : function ( image, status ) {
+
+            $(this).nsProgress('showWithImage', image);
+            $(this).nsProgress('showWithStatus', status);
+
+        },
+        showWithImageAndStatusAndMaskType : function ( image, status, type ) {
+
+            $(this).nsProgress('showWithImage', image);
+            $(this).nsProgress('showWithStatusAndMaskType', status, type);
+
+        },
+        showWithImageAndMaskType : function ( image, type ) {
+
+            $(this).nsProgress('showWithImage', image);
+            $(this).nsProgress('showWithMaskType', type);
+
+        },
+        dismiss : function() {
+
+            // Flush the styles out
+            $('.nsProgress_block').attr('style', '');
+            $(this).find('.nsProgress_text').attr('style', '').html('').hide();
+            $(this).attr('style', '');
+
+            $(this).find('.nsProgress_img').attr('src', $(this).find('.nsProgress_img').attr('data-src') + '/nsLoader.gif');
+
+            $(this).hide();
+            $('.nsProgress_block').hide();
+        }
+    };
+
+    $.fn.nsProgress = function(methodOrOptions) {
+        if ( methods[methodOrOptions] ) {
+            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+            // Default to "init"
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.nsProgress' );
+        }    
+    };
+
+
+})( jQuery );
